8902f7a5479ae3585d2eceb6f81bdbcb4a8fe987
jeffyevangelista <jevangelista@classify.com.ph>
2026-05-22 15:56:12 +0800

    feat(routes): add /event/[eventId] route

---
+++ app/(main)/event/[eventId]/index.tsx
@@ -0,0 +1,12 @@
+import Screen from "@/components/screen";
+import EventDetailsScreen from "@/screens/main/calendar/EventDetailsScreen";
+
+const EventDetailsRoute = () => {
+  return (
+    <Screen>
+      <EventDetailsScreen />
+    </Screen>
+  );
+};
+
+export default EventDetailsRoute;
