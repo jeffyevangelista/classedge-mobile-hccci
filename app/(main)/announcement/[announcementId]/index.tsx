e0de1eb1da2b3b6542e134dad7da29972995e0b4
jeffyevangelista <jevangelista@classify.com.ph>
2026-05-24 01:54:54 +0800

    feat(routes): add /announcement/[announcementId] route

---
+++ app/(main)/announcement/[announcementId]/index.tsx
@@ -0,0 +1,12 @@
+import Screen from "@/components/screen";
+import AnnouncementDetailsScreen from "@/screens/main/announcement/AnnouncementDetailsScreen";
+
+const AnnouncementDetailsRoute = () => {
+  return (
+    <Screen>
+      <AnnouncementDetailsScreen />
+    </Screen>
+  );
+};
+
+export default AnnouncementDetailsRoute;
